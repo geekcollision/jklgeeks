77068e9899ae438cac0ab5a89d9b86f15f91ff05
Juho Vepsalainen <bebraw@gmail.com>
2014-04-15 16:06:16 +0300

    Allow remote access to content

--- serve.js
+++ serve.js
@@ -42,7 +42,12 @@ function serve() {
         app.use(express.errorHandler());
     });
 
+    app.all('*', function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 
+        next();
+    });
 
     var api = rest(app, '/api/v1', {
         geeks: schemas.Geek
